c09e00c4136286a68f1fbd0e68fad22326e9faae
MayuYazawa <121260144+MayuYazawa@users.noreply.github.com>
2023-03-05 22:59:53 +0000

    ゲーム、デバイス・周辺機器一覧、編集、詳細画面作成

--- app/models/device.rb
+++ app/models/device.rb
@@ -10,4 +10,12 @@ class Device < ApplicationRecord
     validates :device_caption
     validates :price
   end
+  
+  def get_image(width, height)
+    unless device_image.attached?
+      file_path = Rails.root.join("app/assets/images/no_image.jpg")
+      image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
+    end
+    device_image.variant(resize_to_fill: [width, height], gravity: :center).processed
+  end
 end

14a0d9b89cba7a6fec6605aed57fff9dadd99147
MayuYazawa <121260144+MayuYazawa@users.noreply.github.com>
2023-03-06 09:10:11 +0000

    管理　ジャンル画面追加

--- app/controllers/admin/device_genres_controller.rb
+++ app/controllers/admin/device_genres_controller.rb
@@ -1,16 +1,42 @@
 class Admin::DeviceGenresController < ApplicationController
   def index
+    @device_genre = DeviceGenre.new
+    @device_genres = DeviceGenre.all
   end
 
   def create
+    @device_genre = DeviceGenre.new(device_genre_params)
+    if @device_genre.save
+      redirect_to request.referer, notice: "ジャンルを追加しました。"
+    else
+      @device_genres = DeviceGenre.all
+      render 'index'
+    end
   end
 
   def edit
+    @device_genre = DeviceGenre.find(params[:id])
   end
 
   def update
+    device_genre = DeviceGenre.find(params[:id])
+    if device_genre.update(device_genre_params)
+      redirect_to admin_device_genres_path
+    else
+      @device_genre = DeviceGenre.find(params[:id])
+      @device_genre.update(device_genre_params)
+      render "edit"
+    end
   end
 
   def destroy
+    @device_genre.destroy
+    redirect_to admin_device_genres_path
+  end
+
+  private
+
+  def device_genre_params
+    params.require(:device_genre).permit(:device_genre_name)
   end
 end
--- app/controllers/admin/game_genres_controller.rb
+++ app/controllers/admin/game_genres_controller.rb
@@ -1,16 +1,42 @@
 class Admin::GameGenresController < ApplicationController
   def index
+    @game_genre = GameGenre.new
+    @game_genres = GameGenre.all
   end
 
   def create
+    @game_genre = GameGenre.new(game_genre_params)
+    if @game_genre.save
+      redirect_to request.referer, notice: "ジャンルを追加しました。"
+    else
+      @game_genres = GameGenre.all
+      render 'index'
+    end
   end
 
   def edit
+    @game_genre = GameGenre.find(params[:id])
   end
 
   def update
+    game_genre = GameGenre.find(params[:id])
+    if game_genre.update(game_genre_params)
+      redirect_to admin_game_genres_path
+    else
+      @game_genre = GameGenre.find(params[:id])
+      @game_genre.update(game_genre_params)
+      render "edit"
+    end
   end
 
   def destroy
+    @game_genre.destroy
+    redirect_to admin_game_genres_path
+  end
+  
+  private
+
+  def game_genre_params
+    params.require(:game_genre).permit(:game_genre_name)
   end
 end

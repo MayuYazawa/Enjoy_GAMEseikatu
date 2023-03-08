45263994d9bf86371f9c5f16445f6b84f0c71d6a
EC2 Default User <ec2-user@ip-172-31-41-70.ap-northeast-1.compute.internal>
2023-03-08 23:00:50 +0000

    コメント系とレビュー

--- app/controllers/public/game_comments_controller.rb
+++ app/controllers/public/game_comments_controller.rb
@@ -3,26 +3,45 @@ class Public::GameCommentsController < ApplicationController
   end
 
   def create
-    @comment = current_user.game_comments.new(game_comment_params)
-    @comment.save
-    redirect_to game_path(@comment.game)
+    @game_comment = GameComment.new(game_comment_params)
+    @game_comment.user_id = current_user.id
+    if @game_comment.save
+      redirect_to games_path, notice: "投稿しました。"
+    else
+      render :new
+    end
+    redirect_to game_path(@game_comment.game)
   end
 
   def update
+    @game_comment = GameComment.find(params[:id])
+    if @game_comment.update(game_comment_params)
+      redirect_to game_path(@game_comment.id)
+    else
+      render "edit"
+    end
   end
 
   def edit
+    @game_comment = GameComment.find(params[:id])
   end
 
   def index
   end
 
   def show
+    @game = Game.find(params[:id])
+    @game_comment = GameComment.new
+    @game_genres = GameGenre.all
+      if params[:game_genre_id].present?
+        @game_genre = GameGenre.find(params[:game_genre_id])
+        @games = @game_genre.page(params[:page]).per(8)
+      end
   end
 
   def destroy
-    @comment = GameComment.find(params[:id])
-    @comment.destroy
+    @game_comment = GameComment.find(params[:id])
+    @game_comment.destroy
   end
 
   private
